31650676e0999c7c6164c7059b28f4f47a0d0fd7
Kenny <kennylevu1@gmail.com>
2024-02-06 10:45:04 -0800

    can read in serial comm, added wiz read and wiz write draft

--- constant.h
+++ constant.h
@@ -1,6 +1,7 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 #define MSK_8 0x80
+#define MSK_16 0x8000
 #define OP_WRITE 0xf0
 #define OP_READ 0x0f
 #endif
--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <8051.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+int buf_counter = 0;
 // #include "spi.h"
-// #include "periph.h"
-// #include "constant.h"
+#include "periph.h"
+#include "constant.h"
 
 // Initial configuartion setting and code sourced from: https://exploreembedded.com/wiki/8051_Family_C_Library
 
-uint8_t flag_input;
- 
 /*-------------------------------------------------------------------------------
                          void spi_init()
 ----------------------------------------------------------------------------------
@@ -26,36 +27,35 @@ uint8_t flag_input;
 void spi_init(void)
 {
     // SCON = SCON & 0x0F; 
-    SCON = 0xD0;    // serial mode 3 8 bit data 1 start and 1 stop bit
-    TMOD = 0x20;    // timer 1 mode 2 and timer 2 mode 0 ---> mode 2 is 16bit timer/counter and thx/tlx are cascaded
+    SCON = 0x50;    // serial mode 3 8 bit data 1 start and 1 stop bit
+    TMOD = 0x20;    // timer 1 mode 2 and timer 2 mode 0 ---> mode 2 is 16bit timer/counter auto reload and thx/tlx are cascaded
     TH1 = 0xfd;     // Load value for 9600 baud --> UART Mode 3 means baud is determined by timer 1's overflow rate 
-    // TL1 = 0xfd;
-    // PCON = 0x00; // SMODE = 0 for 9600 baud
-
     REN = 1;        // serial port initialization
-    // TH0 = 0x00;		// overflow at 65536
-	// TL0 = 0x00;
     EA = 1;			// enable interrupts
 	ES = 1;			// enable serial port interrupts
-    // ET1 = 1; // timer 1 interrupts
-    // TR0 = 1;		// timer on
     TR1 = 1;        // Turn ON timer 1
 
 }
 
-void ser(void) __interrupt(4)
-{
-	RI = 0; // <-- receive interrupt flag
-	dat = SBUF;		// read input buffer
-    ES = 0; // disable serial port interrupt
-	flag_input = 1;
-}
+
+// char getCharacter (void)
+// {
+//     char chr;
+//     while (RI != 1) {;}
+
+//     while(SBUF != '~')
+//     {
+//     chr = chr + SBUF;
+//     }
+//     RI = 0;
+//     return(chr);
+// }
 
 void spi_txchar(char ch)
 {
     SBUF=ch;       // Load the data to be transmitted
-  while(TI==0);    // Wait till the data is trasmitted
-     TI=0;         //Clear the flag for next cycle.
+    while(TI==0);    // Wait till the data is trasmitted
+        TI=0;         //Clear the flag for next cycle.
 }
 
 void spi_txstring(char *string_ptr)
@@ -64,19 +64,129 @@ void spi_txstring(char *string_ptr)
            spi_txchar(*string_ptr++);
 }
 
+unsigned char RX_data(void)
+{
+    unsigned char a;
+    while(RI == 0);
+    RI = 0;
+    a = SBUF;
+    spi_txchar(a);
+    return a;
+}
+
+
+// OP Write Instruction
+void wiz_write(uint16_t addr, uint8_t data) {
+    uint8_t command = OP_WRITE;
+    // send op code
+    for (uint8_t i = 0; i < 8; i++) {
+        if (command & MSK_8) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        CLK = 1;
+        command = command << 1;
+        CLK = 0;
+    }
+
+    // send address field
+    for (uint8_t i = 0; i < 16; i++) {
+        if (addr & MSK_16) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        CLK = 1;
+        addr = addr << 1;
+        CLK = 0;
+    }
+
+    // send data
+    for (uint8_t i = 0; i < 8; i++) {
+        if (data & MSK_8) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        data = 1;
+        command = command << 1;
+        data = 0;
+    }
+
+}
+
+// OP Read Instruction
+void wiz_read(uint16_t addr, uint8_t data) {
+    uint8_t command = OP_READ;
+    // send op code
+    for (uint8_t i = 0; i < 8; i++) {
+        if (command & MSK_8) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        CLK = 1;
+        command = command << 1;
+        CLK = 0;
+    }
+
+    // send address field
+    for (uint8_t i = 0; i < 16; i++) {
+        if (addr & MSK_16) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        CLK = 1;
+        addr = addr << 1;
+        CLK = 0;
+    }
+
+    // send data
+    for (uint8_t i = 0; i < 8; i++) {
+        if (data & MSK_8) {
+            MOSI = 1;
+        }
+        else {
+            MOSI = 0;
+        }
+        data = 1;
+        command = command << 1;
+        data = 0;
+    }
+
+}
+
+// sends byte instructions
+void cmdout(unsigned char cmd) 
+{
+    for (unsigned char i = 0; i < 8; i++) {
+        if (cmd & MSK) {
+            SDA = 1;
+        }
+        else {
+            SDA = 0;
+        }
+        delay(10);
+        SCL = 1;
+        cmd = cmd << 1;  // clock data into sda
+        SCL = 0;
+    }
+}
+
+
 void main(void)
 {
-    // spi_init();
+
     // spi_transmit();
+    spi_init();
 	while (1) {
-        spi_init();
-        if (flag_input == 1) {
-            spi_txstring("Your stuff: ");
-            spi_txtring(char(dat));
-            spi_txstring("\n");
-            ES = 1;
-            flag_input = 0;
-        }
-        // spi_txstring("Hello World\n");
+        RX_data();
     }
 }
