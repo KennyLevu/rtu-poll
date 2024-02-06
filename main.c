fb441bdd1044c7cfc4544a7ff3629ec561b61316
Kenny <kennylevu1@gmail.com>
2024-02-05 17:56:51 -0800

    saving

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <8051.h>
+#include <stdint.h>
 // #include "spi.h"
 // #include "periph.h"
 // #include "constant.h"
 
 // Initial configuartion setting and code sourced from: https://exploreembedded.com/wiki/8051_Family_C_Library
 
+uint8_t flag_input;
+ 
 /*-------------------------------------------------------------------------------
                          void spi_init()
 ----------------------------------------------------------------------------------
@@ -23,22 +26,29 @@
 void spi_init(void)
 {
     // SCON = SCON & 0x0F; 
-    SCON = 0xD0;    // serial mode 1 8 bit data 1 start and 1 stop bit
+    SCON = 0xD0;    // serial mode 3 8 bit data 1 start and 1 stop bit
     TMOD = 0x20;    // timer 1 mode 2 and timer 2 mode 0 ---> mode 2 is 16bit timer/counter and thx/tlx are cascaded
     TH1 = 0xfd;     // Load value for 9600 baud --> UART Mode 3 means baud is determined by timer 1's overflow rate 
     // TL1 = 0xfd;
     // PCON = 0x00; // SMODE = 0 for 9600 baud
 
-    // REN = 1;        // serial port initialization
-    // TMOD |= 0x01;   // mode 1, 16-bit timer - init timer 0 
+    REN = 1;        // serial port initialization
     // TH0 = 0x00;		// overflow at 65536
 	// TL0 = 0x00;
-    // EA = 1;			// enable global interrupts
-	// ES = 1;			// enable serial port interrupt
+    EA = 1;			// enable interrupts
+	ES = 1;			// enable serial port interrupts
     // ET1 = 1; // timer 1 interrupts
     // TR0 = 1;		// timer on
     TR1 = 1;        // Turn ON timer 1
 
+}
+
+void ser(void) __interrupt(4)
+{
+	RI = 0; // <-- receive interrupt flag
+	dat = SBUF;		// read input buffer
+    ES = 0; // disable serial port interrupt
+	flag_input = 1;
 }
 
 void spi_txchar(char ch)
@@ -60,6 +70,13 @@ void main(void)
     // spi_transmit();
 	while (1) {
         spi_init();
-        spi_txstring("Hello World\n");
+        if (flag_input == 1) {
+            spi_txstring("Your stuff: ");
+            spi_txtring(char(dat));
+            spi_txstring("\n");
+            ES = 1;
+            flag_input = 0;
+        }
+        // spi_txstring("Hello World\n");
     }
 }
