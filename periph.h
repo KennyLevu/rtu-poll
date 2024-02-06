4e7c894cabdfd6ac6cf1eeacad1e1e07603a7c44
Kenny <kennylevu1@gmail.com>
2024-02-06 14:53:47 -0800

    fixed bug where ports were backwards

--- periph.h
+++ periph.h
@@ -1,7 +1,7 @@
 #ifndef PERIPH_H
 #define PERIPH_H
-#define CS P0_0 // chip select
-#define CLK P0_1 // CLK pin
-#define MISO P0_2 
-#define MOSI P0_3
+#define CS P0_3 // chip select
+#define CLK P0_2 // CLK pin
+#define MISO P0_1 
+#define MOSI P0_0
 #endif
