7a1426c06b4f8ead0d867fc0954652f6d116503e
Kenny <kennylevu1@gmail.com>
2024-02-15 16:51:59 -0800

    updated py script for simutaneous polling

--- poll.py
+++ poll.py
@@ -1,8 +1,10 @@
 import socket
 import sys
+import threading
 
 
-def poll_udp(ip, port, message, timeout = 3):
+
+def poll_udp(ip, port, message, timeout = 5):
     try:
         # Create a UDP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,10 +15,10 @@ def poll_udp(ip, port, message, timeout = 3):
 
         # Receive response
         data, addr = sock.recvfrom(1024)
-        print("Received:", data.decode())
+        print("UDP Received:", data.decode())
 
     except socket.timeout:
-        print("No response received within", timeout, "seconds.")
+        print("UDP No response received within", timeout, "seconds.")
     except Exception as e:
         print("Error:", e)
     finally:
@@ -36,36 +38,51 @@ def poll_tcp(ip, port, message, timeout = 5):
         sock.send(message.encode())
         # Receive response
         data, addr = sock.recvfrom(1024)
-        print("Received:", data.decode())
+        print("TCP Received:", data.decode())
     except socket.timeout:
-        print("No response received within", timeout, "seconds.")
+        print("TCP No response received within", timeout, "seconds.")
     except Exception as e:
         print("Error:", e)
     finally:
         # Close socket
         sock.close()
 
+# def poll_both(ip, port1, port2, message, timeout = 5):
+
+#     return
+
 if __name__ == "__main__":
     # Check if IP, port, and message are provided as command-line arguments
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <IP> <port> <mode> <message>")
+    if len(sys.argv) != 6:
+        print("Usage: python script.py <IP> <port_udp> <port_tcp> <mode> <message> OR python script.py <IP> <port1> <port2> BOTH <message>")
+
         sys.exit(1)
 
     # Extract IP, port, mode and message from command-line arguments
     ip = sys.argv[1]
-    port = int(sys.argv[2])
-    mode = sys.argv[3]
+    port_udp = int(sys.argv[2])
+    port_tcp = int(sys.argv[3])
+    mode = sys.argv[4]
 
     # Extract message from command-line arguments
-    message = ' '.join(sys.argv[4:])
+    message = ' '.join(sys.argv[5:])
 
     # Send and receive message
     if mode == 'UDP':
-        poll_udp(ip, port, message)
+        poll_udp(ip, port_udp, message)
     if mode == 'TCP':
         # print("test")
-        poll_tcp(ip, port, message)
+        poll_tcp(ip, port_tcp, message)
 
-    # if mode == 'BOTH':
-    #     poll_both(ip, port, message)
+    if mode == 'BOTH':
+    # Create threads for UDP/TCP
+        udp_thread = threading.Thread(target=poll_udp, args=(ip, port_udp, message))
+        tcp_thread = threading.Thread(target=poll_tcp, args=(ip, port_tcp, message))
 
+        # Start both threads
+        udp_thread.start()
+        tcp_thread.start()
+
+        # Wait for both threads to finish
+        udp_thread.join()
+        tcp_thread.join()
